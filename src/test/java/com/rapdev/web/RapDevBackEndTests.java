741b9e6bf538575808ac82a358320153360b111f
reeseboisse <reeseboisse@gmail.com>
2022-03-02 15:23:17 -0500

    Add files via upload

---
+++ src/test/java/com/rapdev/web/RapDevBackEndTests.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+//https://docs.spring.io/spring-framework/docs/current/spring-framework-reference/testing.html#integration-testing-annotations-junit-jupiter
+
+public class RapDevBackEndTests {
+
+    @Test
+    public void loginSuccess() {
+        assertEquals(2, 1 + 1);
+    }
+    
+    @Test
+    public void passwordChange() {
+        assertEquals(2, 1 + 1);
+    }
+    
+    @Test
+    public void apiValidation() {
+        assertEquals(2, 1 + 1);
+    }
+}
